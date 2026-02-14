2bfc665beceab1ffcdc413be7bb065c168b7d4bc
NMaurya563 <maurya@email.com>
2026-02-14 10:14:44 +0530

    Basics Of Python

---
+++ Day-2/Basics.py
@@ -0,0 +1,50 @@
+# print("Hello World")
+# monday = 76
+# tuesday =97
+
+# avg = (monday + tuesday)/2
+# print(avg)
+
+# print(avg,type(avg))
+
+"""name = input("Enter your First Name:")
+LastName = input("Enter your Last Name:")
+print("Hello "+name +" "+LastName)"""
+
+
+"""age= input("Enter your age:")
+age = int(age)
+
+manAge=int(input("Enter your Age:"))
+
+print (age,manAge)"""
+
+"""age = int(input("Enter your age: "))
+
+if age > 18:
+    print("Qualified")
+else:
+    print("Not Qualified")"""
+
+"""marks =int(input())
+
+if marks >90:
+    print ("Exellent")
+elif marks >=70:
+    print("Good")
+elif marks >=50:
+    print("Average")
+elif marks >=35:
+    print ("Pass")
+else:
+    print ("Fail")"""
+
+
+temp =int(input())
+          
+if 25<= temp <=50 :
+    print("Hot")
+elif temp>=10 and temp<=24:
+    print("Cold")
+else:
+    print ("Etremely Cold")
