f5b15bf8a48f439228f3d4f604356527efdf1ccf
NMaurya563 <nitish563mauryarya@gmail.com>
2026-02-16 13:02:05 +0530

    Basics-2

---
+++ Day-5/Basics-2.py
@@ -0,0 +1,40 @@
+"""full_name = "Nitish Maurya"
+age = 22
+address = "Varanasi,Uttar Pradesh"
+
+print(full_name,age,address,sep="_")
+
+print(full_name,age,address,sep=" , ") """
+
+"""i = 0
+
+while i<5:
+    print(i,end=" ")
+    i+=1 
+"""
+
+"""list1 =[3,3,5,2,1]
+
+for i in list1:
+    print(i,end=" ")
+print()
+
+for i in range(1,11):
+    print(i,end=" ")
+print()
+
+for i in range(5,11):
+    print(i,end=" ")
+print()
+
+for i in range(1,11,2):
+    print(i,end=" ")
+print()"""
+
+def  addTwoNumbers(a,b):
+    sum = a+b
+    return sum
+
+ans = addTwoNumbers(5,9)
+print(ans)
+
