ca5f4f572267c4ed576ae04c1dfd9982189bb132
NMaurya563 <nitish563mauryarya@gmail.com>
2026-02-19 22:02:00 +0530

    Leetcode Problem E

---
+++ Day-7/LC-Qno-509.py
@@ -0,0 +1,12 @@
+# 509. Fibonacci Number
+
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==0 or n==1:
+            return n
+        return self.fib(n-1)+self.fib(n-2)
+        
