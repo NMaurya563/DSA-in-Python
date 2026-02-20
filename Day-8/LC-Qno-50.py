6ba0fb91ead7fdfdf8fc3009cd5010cdf493bc33
NMaurya563 <nitish563mauryarya@gmail.com>
2026-02-20 22:46:26 +0530

    Leetcode Problem E

---
+++ Day-8/LC-Qno-50.py
@@ -0,0 +1,17 @@
+# 50. Pow(x, n) 
+
+class Solution:
+    def myPow(self, x, n):
+        if n == 0:
+            return 1.0
+        
+        if n < 0:
+            x = 1 / x
+            n = -n
+        
+        half = self.myPow(x, n // 2)
+        
+        if n % 2 == 0:
+            return half * half
+        else:
+            return x * half * half
