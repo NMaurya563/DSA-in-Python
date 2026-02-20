4299f2d0546829d91edacfd9874bdf7c690a65f8
NMaurya563 <nitish563mauryarya@gmail.com>
2026-02-20 22:09:39 +0530

    LCM Concept

--- Day-8/GCD(HCF).py
+++ Day-8/GCD(HCF).py
@@ -4,3 +4,9 @@ def gcd(a,b):
     return gcd(b,a%b)
 
 print(gcd(15,50))
+
+# there is no efficient way to find the LCM . We can find the LCM using the GCD. The formula is LCM(a,b) = (a*b)/GCD(a,b)
+
+def lcm(a,b):
+    return (a*b)//gcd(a,b)
+print(lcm(15,50))
