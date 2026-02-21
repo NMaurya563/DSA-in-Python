f87279b53bcf9203cb4fb1d994c960b12d7c5d76
NMaurya563 <nitish563mauryarya@gmail.com>
2026-02-21 23:16:30 +0530

    Leetcode Problem M

---
+++ Day-9/LC-Qno-80.py
@@ -0,0 +1,16 @@
+# 80. Remove Duplicates from Sorted Array II
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n=len(nums)
+        start = 1
+        for i in range(2,n):
+            if nums[i]!=nums[start-1]:
+                start+=1
+                nums[start]=nums[i]
+        return start+1
+        
