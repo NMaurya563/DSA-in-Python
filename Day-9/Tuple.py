f6bae982b52e8c34ea71683ce29a161adedc8eaa
NMaurya563 <nitish563mauryarya@gmail.com>
2026-02-21 22:21:17 +0530

    Tuple

---
+++ Day-9/Tuple.py
@@ -0,0 +1,14 @@
+tuple1= ("apple","banana","cherry",54,68,True,3.125)
+
+print(tuple1)
+print(tuple1[0])
+print(tuple1[-1])
+print(len(tuple1))
+
+tuple2=(1,2,3,4,5)
+tuple3 = tuple2 
+
+print(tuple2)
+print(tuple3)
+
+print(tuple2[2:4]) # This will print the elements from index 2 to index 3 (4 is not included).
