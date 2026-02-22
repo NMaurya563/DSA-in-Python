7c8521e0581c0e14f2f83095fa326e92d17b4b7a
NMaurya563 <nitish563mauryarya@gmail.com>
2026-02-22 21:46:07 +0530

    Leetcode Problem E

---
+++ Day-10/LC-Qno-905.py
@@ -0,0 +1,18 @@
+# 905. Sort Array By Parity
+
+class Solution(object):
+    def sortArrayByParity(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        n= len(nums)
+        start = 0
+
+        for i in range(n):
+            if nums[i]%2==0:
+                temp=nums[i]
+                nums[i]=nums[start]
+                nums[start]=temp
+                start+=1
+        return nums
