395f23181d09ced14da4f4f41b7518f48cc7cfef
NMaurya563 <nitish563mauryarya@gmail.com>
2026-02-23 22:11:02 +0530

    Leetcode Problem E

---
+++ Day-11/LC-Qno-1108.py
@@ -0,0 +1,10 @@
+# 1108. Defanging an IP Address
+
+class Solution(object):
+    def defangIPaddr(self, address):
+        """
+        :type address: str
+        :rtype: str
+        """
+        return address.replace(".","[.]")
+        
