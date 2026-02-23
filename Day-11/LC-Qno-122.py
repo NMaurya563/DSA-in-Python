e2f4ec250c4ac912898420df5e6987c392d3dced
NMaurya563 <nitish563mauryarya@gmail.com>
2026-02-23 21:56:20 +0530

    Leetcode Problem M

---
+++ Day-11/LC-Qno-122.py
@@ -0,0 +1,14 @@
+# 122. Best Time to Buy and Sell Stock II
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        n = len(prices)
+        for i in range(n-1):
+            if prices[i+1]>prices[i]:
+                profit+=(prices[i+1]-prices[i])
+        return profit
