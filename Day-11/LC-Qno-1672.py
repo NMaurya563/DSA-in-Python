f4c0a452c69820d6e872073b1587cd568b10a50a
NMaurya563 <nitish563mauryarya@gmail.com>
2026-02-23 21:54:00 +0530

    Leetcode Problem E

---
+++ Day-11/LC-Qno-1672.py
@@ -0,0 +1,14 @@
+# 1672. Richest Customer Wealth
+
+class Solution(object):
+    def maximumWealth(self, accounts):
+        """
+        :type accounts: List[List[int]]
+        :rtype: int
+        """
+        ans = 0
+        for account in accounts:
+            ans = max(ans,sum(account))
+        
+        return ans
+        
