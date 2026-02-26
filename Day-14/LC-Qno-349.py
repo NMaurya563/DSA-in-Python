b0dd11090bb213bd18c44c698a17da7349b6e5df
NMaurya563 <nitish563mauryarya@gmail.com>
2026-02-26 23:42:10 +0530

    Leetcode Problem E

---
+++ Day-14/LC-Qno-349.py
@@ -0,0 +1,14 @@
+# 349. Intersection of Two Arrays
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        set1=set(nums1)
+        set2=set(nums2)
+
+        return list(set1.intersection(set2)) 
+        # return list(set1 & set2) # This will also give the intersection of two sets
