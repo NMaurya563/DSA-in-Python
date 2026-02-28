929b2580629d3f9524c27f57b373dcb6ed87c75d
NMaurya563 <nitish563mauryarya@gmail.com>
2026-02-28 11:53:25 +0530

    Leetcode Problem E

---
+++ Day-16/LC-Qno-242.py
@@ -0,0 +1,35 @@
+# 242. Valid Anagram
+
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+
+        freq ={}
+        for i in s:
+            if i in freq:
+                freq[i]+= 1
+            else:
+                freq[i]=1
+
+        
+        for i in t:
+            if i in freq:
+                freq[i]-= 1
+            else:
+                return False
+
+        for i in freq.values():
+            if i!=0:
+                return False
+                    
+                
+        return True
+
+        
+        
