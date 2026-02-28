b7412f2af013e9e0fb4600c0a2e87f3271486474
NMaurya563 <nitish563mauryarya@gmail.com>
2026-02-28 12:11:27 +0530

    Leetcode Problem M

---
+++ Day-16/LC-Qno-49.py
@@ -0,0 +1,23 @@
+# 49. Group Anagrams 
+
+class Solution(object):
+    def sortString(self,s):
+        s1=list(s)
+        s1.sort()
+        return "".join(s1)
+
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        dict1 = {}
+
+        for s in strs:
+            key=self.sortString(s)
+            if key in dict1:
+                dict1[key].append(s)
+            else:
+                dict1[key]=[s]
+        return list(dict1.values())
+        
