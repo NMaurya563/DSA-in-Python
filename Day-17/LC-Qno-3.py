41604829c92957a3da139db4bef8e430df348eb0
NMaurya563 <nitish563mauryarya@gmail.com>
2026-03-03 22:55:27 +0530

    Leetcode Problem M

---
+++ Day-17/LC-Qno-3.py
@@ -0,0 +1,31 @@
+# 3. Longest Substring Without Repeating Characters
+
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        ans = 1
+        n = len(s)
+        if n==0:
+            return 0
+        set1 = set({})
+        set1.add(s[0])
+
+        i = 0
+        j = 1
+
+        while j<n:
+            while s[j] in set1:
+                set1.remove(s[i])
+                i+=1
+            set1.add(s[j])
+            j+=1
+            ans = max(ans,(j-i))
+
+        return ans
+            
+         
+        
