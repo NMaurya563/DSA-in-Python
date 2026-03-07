cc152a065561348d924f212864ec9765a4d2648d
NMaurya563 <nitish563mauryarya@gmail.com>
2026-03-07 23:44:24 +0530

    Leetcode Problem E

---
+++ Day-21/LC-Qno-561.py
@@ -0,0 +1,12 @@
+# 561. Array Partition
+
+
+class Solution(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+
+    return sum(nums[::2])
