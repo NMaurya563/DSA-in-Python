b46bdee51880e04df4922f1e785c177ff3b08f54
NMaurya563 <nitish563mauryarya@gmail.com>
2026-03-11 22:14:31 +0530

    Leetcode Problem E

---
+++ Day-25/LC-Qno-268.py
@@ -0,0 +1,13 @@
+# 268. Missing Number
+
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        
+        sum1=n*(n+1)//2
+        sum2=sum(nums)
+        return sum1-sum2
